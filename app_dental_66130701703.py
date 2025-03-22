c0e386b3ee23fa06137e6caf37c061f7300036da
kitti1989 <kittipat.puan@gmail.com>
2025-03-22 18:43:47 +0700

    Add files via upload

---
+++ app_dental_66130701703.py
@@ -0,0 +1,13 @@
+from ultralytics import solutions
+import gdown
+
+url = "https://drive.google.com/uc?id=1bcge0W2AT9KWRlGXg-S4BS75cCA4TXvw"
+
+model = 'yolo.pt'
+gdown.download(url, model, quiet=False)
+
+inf = solutions.Inference(
+    model=model,  # you can use any model that Ultralytics support, i.e. YOLO11, or custom trained model
+)
+
+inf.inference()
